e2b81f2305697e5127c0ef1cdc2f653dda4a61eb
Kentaro Ozeki <32771324+ozekik@users.noreply.github.com>
2023-12-11 00:00:00 +0900

    feat: add CodelistParser

--- plateaukit/parsers.py
+++ plateaukit/parsers.py
@@ -60,6 +60,33 @@ class CityGML:
     city_objects: list[CityObject]
 
 
+class CodelistParser:
+    """A parser for codelists."""
+
+    def __init__(self):
+        pass
+
+    def parse(self, infile):
+        tree = etree.parse(infile)
+        root = tree.getroot()
+
+        result = dict()
+
+        for el in root.iterfind(".//gml:dictionaryEntry/gml:Definition", nsmap):
+            # print(el)
+
+            el_key = el.find("./gml:name", nsmap)
+            key = el_key.text if el_key is not None else None
+
+            el_value = el.find("./gml:description", nsmap)
+            value = el_value.text if el_value is not None else None
+
+            if key is not None:
+                result[key] = value
+
+        return result
+
+
 class GeometryParser:
     """A parser for CityGML geometries.
 
