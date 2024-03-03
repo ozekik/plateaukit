9d90e933b003ac44314a92145f4b03bf9b7a9d62
Kentaro Ozeki <32771324+ozekik@users.noreply.github.com>
2024-03-04 00:00:00 +0900

    update: remove unused deps and make quantized-mesh-encoder optional

--- plateaukit/db.py
+++
@@ -1,6 +0,0 @@
-from tortoise import Tortoise
-
-
-async def init(DB_URL):
-    await Tortoise.init(db_url=DB_URL, modules={"models": ["plateaukit.models"]})
-    await Tortoise.generate_schemas()
--- plateaukit/generators/utils.py
+++ plateaukit/generators/utils.py
@@ -1,7 +1,4 @@
-import sys
 from pathlib import Path
-
-from tqdm import tqdm
 
 from plateaukit.logger import logger
 
@@ -12,14 +9,13 @@ def geojson_to_gpkg(
 ):
     import geopandas as gpd
 
-    with tqdm(infiles) as pbar:
-        for infile in pbar:
-            logger.debug(infile)
-            with open(infile, "r") as f:
-                # print(infile)
-                df = gpd.read_file(f, encoding="tuf-8")
-                # print(df)
-                if not Path(outfile).exists():
-                    df.to_file(outfile, driver="GPKG", mode="w")
-                else:
-                    df.to_file(outfile, driver="GPKG", mode="a")
+    for infile in infiles:
+        logger.debug(infile)
+        with open(infile, "r") as f:
+            # print(infile)
+            df = gpd.read_file(f, encoding="tuf-8")
+            # print(df)
+            if not Path(outfile).exists():
+                df.to_file(outfile, driver="GPKG", mode="w")
+            else:
+                df.to_file(outfile, driver="GPKG", mode="a")
