6c7335f0ee832575084e4531de20b905b84ddb42
Kentaro Ozeki <32771324+ozekik@users.noreply.github.com>
2024-03-29 00:00:00 +0900

    add tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,15 +2,38 @@ import subprocess
 import sys
 import unittest
 
+from click.testing import CliRunner
+
+from plateaukit.cli.cli import cli
+
 
 class CLITest(unittest.TestCase):
-    def test_cli(self):
+    def test_shell_cli(self):
         if sys.platform == "win32":
             cp = subprocess.run("tests\\test_cli.bat", shell=True)
         else:
             cp = subprocess.run("tests/test_cli.sh", shell=True)
 
         self.assertEqual(cp.returncode, 0)
+
+    def test_list(self):
+        runner = CliRunner()
+        result = runner.invoke(cli, ["list"])
+        assert result.exit_code == 0
+        # assert "..." in result.output
+
+    def test_config(self):
+        runner = CliRunner()
+        result = runner.invoke(cli, ["config"])
+        assert result.exit_code == 0
+
+    def test_info(self):
+        runner = CliRunner()
+        # TODO: Installed case
+
+        # Case for datasets not installed
+        result = runner.invoke(cli, ["info", "plateau-28225-asago-shi-2022"])
+        assert result.exit_code == 0
 
 
 if __name__ == "__main__":
--- tests/test_cli.sh
+++ tests/test_cli.sh
@@ -11,3 +11,5 @@ plateaukit () {
 # TODO: Test remote install
 plateaukit install plateau-30422-taiji-cho-2021 --force --local ./fixtures/30422_taiji-cho_2021_citygml_2_op.zip
 plateaukit generate-cityjson --dataset plateau-30422-taiji-cho-2021 /tmp/output.city.json
+plateaukit info plateau-30422-taiji-cho-2021
+plateaukit info plateau-28225-asago-shi-2022
