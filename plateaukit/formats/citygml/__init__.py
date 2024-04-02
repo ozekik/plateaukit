88a89bd5291867dae4e45e92173f3669076c121f
Kentaro Ozeki <32771324+ozekik@users.noreply.github.com>
2024-04-03 00:00:00 +0900

    chore: refactoring

--- plateaukit/formats/citygml/__init__.py
+++ plateaukit/formats/citygml/__init__.py
@@ -1,10 +1,21 @@
-from plateaukit.formats.citygml.city_object import CityObject  # noqa: F401
-from plateaukit.formats.citygml.citygml import CityGML  # noqa: F401
-from plateaukit.formats.citygml.citygml_dataset import CityGMLDataset  # noqa: F401
+from plateaukit.formats.citygml.city_object import CityObject
+from plateaukit.formats.citygml.citygml import CityGML
+from plateaukit.formats.citygml.citygml_dataset import CityGMLDataset
 from plateaukit.formats.citygml.parsers import (
-    CityGMLParser,  # noqa: F401
-    PLATEAUCityGMLParser,  # noqa: F401
-    CityObjectParser,  # noqa: F401
-    PLATEAUCityObjectParser,  # noqa: F401
-    CodelistParser,  # noqa: F401
+    CityGMLParser,
+    CityObjectParser,
+    CodelistParser,
+    PLATEAUCityGMLParser,
+    PLATEAUCityObjectParser,
 )
+
+__all__ = [
+    "CityObject",
+    "CityGML",
+    "CityGMLDataset",
+    "CityGMLParser",
+    "CityObjectParser",
+    "CodelistParser",
+    "PLATEAUCityGMLParser",
+    "PLATEAUCityObjectParser",
+]
