f55ed12936316f2407ce1a48ec5e01f02112c020
Kentaro Ozeki <32771324+ozekik@users.noreply.github.com>
2024-10-16 00:00:00 +0900

    update(exporters): (wip) quantized_mesh

--- plateaukit/exporters/quantized_mesh.py
+++ plateaukit/exporters/quantized_mesh.py
@@ -1,6 +1,9 @@
+# %%
+import fast_simplification
 import numpy as np
 from bidict import bidict
 from lxml import etree
+from pyproj import Transformer
 
 from plateaukit.constants import default_nsmap
 from plateaukit.logger import logger
@@ -37,22 +40,22 @@ class VerticesMap:
 
 def triangles_from_gml(infiles, precision=8):
     try:
-        from quantized_mesh_encoder import encode
+        from quantized_mesh_encoder.encode import encode
     except ImportError:
         logger.error("quantized_mesh_encoder is not installed")
         return
 
     vertices_map = VerticesMap()
 
-    indices = []
+    faces = []
 
     for infile in infiles:
         with open(infile, "r") as f:
             root = etree.parse(f)
-            elems_Triangle = root.iterfind(f".//{default_nsmap['gml']}Triangle")
+            elems_Triangle = root.iterfind(f".//{{{default_nsmap['gml']}}}Triangle")
             for elem_Triangle in elems_Triangle:
                 elem_posList = elem_Triangle.find(
-                    f"./{default_nsmap['gml']}exterior/{default_nsmap['gml']}LinearRing/{default_nsmap['gml']}posList"
+                    f"./{{{default_nsmap['gml']}}}exterior/{{{default_nsmap['gml']}}}LinearRing/{{{default_nsmap['gml']}}}posList"
                 )
                 pos_list = parse_posList(elem_posList.text)
                 # Cut off the last element identical to the first
@@ -62,12 +65,39 @@ def triangles_from_gml(infiles, precision=8):
                     vertices_map.to_index(v) for v in triangle_vertices
                 )
                 # print(indices)
-                indices.append(triangle_indices)
+                faces.append(triangle_indices)
 
     vertices = np.array(vertices_map.vertices)
-    indices = np.array(indices)
-    print(vertices)
-    print(indices)
+    faces = np.array(faces)
 
-    with open("output.terrain", "wb") as f:
-        encode(f, vertices, indices)
+    # Japan to Web Mercator
+    transformer = Transformer.from_crs("EPSG:4612", "EPSG:3857")
+    vertices = np.array([transformer.transform(*v) for v in vertices], dtype=np.float32)
+    # Transform xy only, keep z
+    # vertices = np.array(
+    #     [(*transformer.transform(*v[:2]), v[2]) for v in vertices],
+    #     dtype=np.float32,
+    # )
+    # faces
+    print(vertices)
+    print(faces)
+    print(len(vertices), len(faces))
+    vertices_out, faces_out = fast_simplification.simplify(
+        vertices, faces, 0.9, verbose=True
+    )
+    print(len(vertices_out), len(faces_out))
+
+    with open("/tmp/output.terrain", "wb") as f:
+        encode(f, vertices_out, faces_out)
+
+
+def main():
+    import glob
+
+    infiles = glob.glob("/tmp/hakone-dem/*.gml")
+    print(infiles)
+    triangles_from_gml(infiles)
+
+
+if __name__ == "__main__":
+    main()
