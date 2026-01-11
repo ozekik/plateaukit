b41fbf1c3437d314cde8e12bc2938b309ce5f8f9
Kentaro Ozeki <32771324+ozekik@users.noreply.github.com>
2026-01-12 00:00:00 +0900

    add tests for v4 spec

--- tests/test_cli.sh
+++ tests/test_cli.sh
@@ -24,7 +24,20 @@ plateaukit info plateau-30422-taiji-cho-2021.v2
 plateaukit info plateau-28225-asago-shi-2022.v2
 
 # Spec v3
-plateaukit install plateau-30422-taiji-cho-2021 --force --local ./fixtures/30422_taiji-cho_city_2021_citygml_4_op.zip
+plateaukit install plateau-30422-taiji-cho-2021.v3 --force --local ./fixtures/30422_taiji-cho_city_2021_citygml_4_op.zip
+plateaukit prebuild plateau-30422-taiji-cho-2021.v3 -t bldg
+
+plateaukit export-geojson --dataset plateau-30422-taiji-cho-2021.v3 /tmp/output.geo.json
+plateaukit export-cityjson --dataset plateau-30422-taiji-cho-2021.v3 /tmp/output.city.json
+
+plateaukit export-geojson --dataset plateau-30422-taiji-cho-2021.v3 /tmp/output.geo.jsonl --seq
+plateaukit export-cityjson --dataset plateau-30422-taiji-cho-2021.v3 /tmp/output.city.jsonl --seq
+
+plateaukit info plateau-30422-taiji-cho-2021.v3
+plateaukit info plateau-28225-asago-shi-2022.v3
+
+# Spec v4
+plateaukit install plateau-30422-taiji-cho-2021 --force --local ./fixtures/30422_taiji-cho_city_2021_citygml_5_op.zip
 plateaukit prebuild plateau-30422-taiji-cho-2021 -t bldg
 
 plateaukit export-geojson --dataset plateau-30422-taiji-cho-2021 /tmp/output.geo.json
